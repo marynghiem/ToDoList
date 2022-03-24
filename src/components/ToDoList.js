db3b1061c0c73910faaed7fe824f58b9cfc53162
marynghiem <marynghiem46@gmail.com>
2022-03-24 14:56:51 -0700

    displayed to do items in a list and added handle click to delete to do

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -11,11 +11,29 @@ export const ToDoList = () => {
   const handleAddToDo = () => {
     setToDo([...todo, currentToDo]);
   };
+
+  const handleRemoveToDo = (targetIndex) => {
+    setToDo((prev) => {
+      return prev.filter((todo, index) => index !== targetIndex);
+    });
+  };
+
   console.log(todo);
   return (
     <div>
+      <h1>To do</h1>
+      <h3>What needs to be done?</h3>
       <input type="text" onChange={handleToDo} value={currentToDo}></input>
       <button onClick={handleAddToDo}>Add</button>
+      <h3>{todo.length} tasks remaining</h3>
+      <ul>
+        {todo.map((item, index) => (
+          <>
+            <li>{item}</li>
+            <button onClick={() => handleRemoveToDo(index)}>Delete {item}</button>
+          </>
+        ))}
+      </ul>
     </div>
   );
 };
