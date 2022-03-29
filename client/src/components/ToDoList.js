485f6e3656f3918ad098036c9c34d386f52199bd
marynghiem <marynghiem46@gmail.com>
2022-03-28 23:36:32 -0700

    added state for edit button and onclicks

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -4,6 +4,8 @@ import "../css/ToDoList.css";
 export const ToDoList = () => {
   const [todo, setToDo] = useState([]);
   const [currentToDo, setCurrentToDo] = useState("");
+  const [editToDo, setEditToDo] = useState(false);
+  const [currentEditToDo, setCurrentEditToDo] = useState("");
 
   const handleToDo = (e) => {
     setCurrentToDo(e.target.value);
@@ -11,12 +13,32 @@ export const ToDoList = () => {
   const handleAddToDo = () => {
     setToDo([...todo, currentToDo]);
   };
+  const handleEditToDoValue = (e) => {
+    setCurrentEditToDo(e.target.value);
+  };
+
+  const handleEditToDo = () => {
+    if (editToDo === false) {
+      setEditToDo(true);
+    } else if (editToDo === true) {
+      setEditToDo(false);
+    }
+  };
+
+  const handleSubmitEditTodo = (todoIndex) => {
+    setToDo((prev) => {
+      return prev.filter((todo, index) => index !== todoIndex);
+    });
+    setToDo([...todo, currentEditToDo]);
+    setEditToDo(false);
+  };
 
   const handleRemoveToDo = (targetIndex) => {
     setToDo((prev) => {
       return prev.filter((todo, index) => index !== targetIndex);
     });
   };
+  console.log(editToDo);
 
   console.log(todo);
   return (
@@ -32,15 +54,26 @@ export const ToDoList = () => {
       </div>
       <h3>{todo.length} tasks remaining</h3>
       <ul>
-        {todo.map((item, index) => (
-          <>
-            <li>
-              <input type="checkbox" name={`${item}'s checkbox`} value={item} />
-              <label for={`${item}'s checkbox`}> {item}</label>
-            </li>
-            <button onClick={() => handleRemoveToDo(index)}>Delete {item}</button>
-          </>
-        ))}
+        {todo.map((item, index) =>
+          editToDo === false ? (
+            <>
+              <li>
+                <input type="checkbox" name={`${item}'s checkbox`} value={item} />
+                <label for={`${item}'s checkbox`}> {item}</label>
+              </li>
+              <button onClick={handleEditToDo}>Edit {item}</button>
+              <button onClick={() => handleRemoveToDo(index)}>Delete {item}</button>
+            </>
+          ) : (
+            <>
+              {" "}
+              <li>
+                <input type="text" onChange={handleEditToDoValue} value={currentEditToDo}></input>
+                <button onClick={handleSubmitEditTodo}>Submit</button>
+              </li>
+            </>
+          )
+        )}
       </ul>
     </div>
   );
