380bca1ef88593c7aaaa74e3b72857103b4766de
marynghiem <marynghiem46@gmail.com>
2022-03-30 23:39:49 -0700

    added postgres and connected the databases

--- server/index.js
+++ server/index.js
@@ -1,6 +1,35 @@
 const express = require("express"); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//ROUTES//
+
+//create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", [description]);
+
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+// delete a todo
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
