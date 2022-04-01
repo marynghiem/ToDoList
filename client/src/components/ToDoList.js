ec1ea71f7b37f0c8d25fad13d3814dc2cbe1e183
marynghiem <marynghiem46@gmail.com>
2022-04-01 13:39:39 -0700

    was able to send front end descriptions to the back end

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -2,76 +2,45 @@ import React, { useState, Fragment } from "react";
 import "../css/ToDoList.css";
 
 export const ToDoList = () => {
-  const [todo, setToDo] = useState([]);
-  const [currentToDo, setCurrentToDo] = useState("");
+  const [description, setDescription] = useState("");
   const [editToDo, setEditToDo] = useState([]); //keep track of index rather than truth values
   const [currentEditToDo, setCurrentEditToDo] = useState("");
 
-  const handleToDo = (e) => {
-    setCurrentToDo(e.target.value);
-  };
-  const handleAddToDo = () => {
-    setToDo([...todo, currentToDo]);
-  };
-  const handleEditToDoValue = (e) => {
-    setCurrentEditToDo(e.target.value);
+  //adding description or sending data out
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
+    }
   };
 
-  const handleEditToDo = (targetIndex) => {
-    setEditToDo([...editToDo, targetIndex]);
-  };
-
-  const handleSubmitEditTodo = (targetIndex) => {
-    const newToDoArr = [...todo];
-    newToDoArr[targetIndex] = currentEditToDo;
-    setToDo([...newToDoArr]);
-  };
-
-  const handleRemoveToDo = (targetIndex) => {
-    setToDo((prev) => {
-      return prev.filter((todo, index) => index !== targetIndex);
-    });
-  };
-  console.log(editToDo);
-
-  console.log(todo);
   return (
     <Fragment>
       <h1 className="text-center mt-5">To do</h1>
       <h3>What needs to be done?</h3>
-      <form className="d-flex mt-5">
-        <input type="text" className="form-control" onChange={handleToDo} value={currentToDo}></input>
-        <button className="btn btn-success" onClick={handleAddToDo}>
-          Add
-        </button>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></input>
+        <button className="btn btn-success">Add</button>
       </form>
       <div>
         <button>Show all tasks</button>
         <button>Show active tasks</button>
         <button>Show completed tasks</button>
       </div>
-      <h3>{todo.length} tasks remaining</h3>
-      <ul>
-        {todo.map((item, index) =>
-          !editToDo.includes(index) ? (
-            <div key={`${item}${index}`}>
-              <li>
-                <input type="checkbox" name={`${item}'s checkbox`} value={item} />
-                <label htmlFor={`${item}'s checkbox`}> {item}</label>
-              </li>
-              <button onClick={() => handleEditToDo(index)}>Edit {item}</button>
-              <button onClick={() => handleRemoveToDo(index)}>Delete {item}</button>
-            </div>
-          ) : (
-            <div key={`${item}${index}`}>
-              <li>
-                <input type="text" onChange={handleEditToDoValue} value={currentEditToDo}></input>
-                <button onClick={() => handleSubmitEditTodo(index)}>Submit</button>
-              </li>
-            </div>
-          )
-        )}
-      </ul>
     </Fragment>
   );
 };
