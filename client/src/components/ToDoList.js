fa7e105e725cbd2a6a39bf412af49943885f9390
marynghiem <marynghiem46@gmail.com>
2022-04-01 15:29:34 -0700

    added bootstrap modal

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -4,7 +4,6 @@ import "../css/ToDoList.css";
 export const ToDoList = () => {
   const [description, setDescription] = useState("");
   const [todos, setTodos] = useState([]);
-  const [currentEditToDo, setCurrentEditToDo] = useState("");
 
   //adding description or sending data out
 
@@ -82,7 +81,29 @@ export const ToDoList = () => {
             {todos.map((todo) => (
               <tr key={todo.todo_id}>
                 <td>{todo.description}</td>
-                <td>Edit</td>
+                <td>
+                  <Fragment>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+                      Edit
+                    </button>
+                    <div class="modal" id="myModal">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h4 class="modal-title">Edit Todo</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                          </div>
+                          <div class="modal-body">Modal body..</div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">
+                              Close
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Fragment>
+                </td>
                 <td>
                   <button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>
                     Delete
