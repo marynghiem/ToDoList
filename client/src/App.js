4702cd4a2e3f631b18330fe95289323f0a5e6319
marynghiem <marynghiem46@gmail.com>
2022-05-05 15:36:39 -0700

    added navbar for project

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,16 @@ import "./App.css";
 import { ToDoList } from "./components/ToDoList";
 import React, { Fragment } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./components/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Fragment>
-            <ToDoList />
-          </Fragment>
-        }
-      ></Route>
-    </Routes>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<ToDoList />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavBar } from "./NavBar";
+
+export const Home = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  );
+};
