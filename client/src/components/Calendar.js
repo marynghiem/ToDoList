6d2b823c71ac38a438b002565448eface9b8e5e7
marynghiem <marynghiem46@gmail.com>
2022-05-31 17:11:49 -0700

    created get month name function

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -80,6 +80,26 @@ const getDays = (month, year) => {
   return ar[month];
 };
 
+const getMonthName = (month) => {
+  let ar = new Array(12);
+
+  ar[0] = "January";
+  ar[1] = "February";
+  ar[2] = "March";
+  ar[3] = "April";
+  ar[4] = "May";
+  ar[5] = "June";
+  ar[6] = "July";
+  ar[7] = "August";
+  ar[8] = "September";
+  ar[9] = "October";
+  ar[10] = "November";
+  ar[11] = "December";
+
+  // return name of specified month (parameter)
+  return ar[month];
+};
+
 export const Calendar = () => {
   const [timeLength, setTimeLength] = useState("Month");
   const [month, setMonth] = useState("January");
