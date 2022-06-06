a48b74fbc5f50774c274ccb2bc82ba0d2b157084
marynghiem <marynghiem46@gmail.com>
2022-06-06 11:37:26 -0700

    added react calendar for comparision

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Calendar } from "./Calendar";
 import { NavBar } from "./NavBar";
+import Calendar from "react-calendar";
+import { MarysCalendar } from "./MarysCalendar";
 
 export const Home = () => {
   return (
     <div>
       <NavBar></NavBar>
       <Calendar></Calendar>
+      <MarysCalendar />
     </div>
   );
 };
---
+++ client/src/components/MarysCalendar.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "../css/Calendar.css";
+
+const MONTHS_IN_A_YEAR = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "Decemeber",
+];
+const DAYS_OF_THE_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const COLUMNS_OF_DAYS = 7;
+const ROWS_OF_WEEKS = 6;
+
+const create2DArray = (rowNum, columnNum) => {
+  const totalArray = [];
+  for (let i = 0; i < rowNum; i++) {
+    const rows = [];
+    for (let j = 0; j < columnNum; j++) {
+      rows.push(0);
+    }
+    totalArray.push(rows);
+  }
+  return totalArray;
+};
+
+console.log(create2DArray(ROWS_OF_WEEKS, COLUMNS_OF_DAYS));
+
+export const MarysCalendar = () => {
+  const [timeLength, setTimeLength] = useState("Month");
+  const [month, setMonth] = useState("January");
+
+  const handleMonthChange = (e) => {
+    setMonth(e.target.value);
+  };
+
+  return (
+    <>
+      <select name="month" id="month" onChange={handleMonthChange}>
+        {MONTHS_IN_A_YEAR.map((month) => (
+          <option value={month}>{month}</option>
+        ))}
+      </select>
+      <select name="time-frame" id="time-frame">
+        <option value="weekly">Weekly</option>
+        <option value="monthly">Monthly</option>
+        <option value="yearly">Yearly</option>
+      </select>
+      <div className="calendar-container">
+        <h3 className="month-title">{month}</h3>
+        <div className="day-of-the-week-container">
+          {DAYS_OF_THE_WEEK.map((day) => (
+            <div className="day-of-the-week">{day}</div>
+          ))}
+        </div>
+        {create2DArray(ROWS_OF_WEEKS, COLUMNS_OF_DAYS).map((week) =>
+          week.map((day) => <div className="day-box">hello</div>)
+        )}
+      </div>
+    </>
+  );
+};
