ffc014a9b1d97eb54b60f691b0e62ff9a916ebbd
marynghiem <marynghiem46@gmail.com>
2022-06-07 12:32:33 -0700

    added nav bar to all components and added css to calendar

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import { ToDoList } from "./components/ToDoList";
 import React, { Fragment } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./components/Home";
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<ToDoList />} />
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { NavBar } from "./NavBar";
 import { MarysCalendar } from "./MarysCalendar";
 
 export const Home = () => {
   return (
     <div>
-      <NavBar></NavBar>
       <MarysCalendar />
     </div>
   );
